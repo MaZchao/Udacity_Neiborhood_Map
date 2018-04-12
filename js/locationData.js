350b3b3c656bf02e8f54742ec71d80e658e1710c
Chris <mayuexiao900@gmail.com>
2018-04-12 16:33:38 +0800

    Feature：新增功能，美化地图。 1、输入文本筛选list和marker。 2、marker颜色自定义以及hover颜色。 3、地图样式修改（google官方获取）。 4、将locationData单独放在一个文件里面。

---
+++ js/locationData.js
@@ -0,0 +1,23 @@
+// my fake location data
+const locationData = [
+  {
+    title: 'Tianfu Square',
+    location: { lat: 104.065837, lng: 30.657349 },
+  },
+  {
+    title: 'Martial Marquis Memorial Temple',
+    location: { lat: 104.047994, lng: 30.646094 },
+  },
+  {
+    title: 'Sichuan Univercity',
+    location: { lat: 104.083748, lng: 30.630869 },
+  },
+  {
+    title: 'Du Fu Thatched Cottage',
+    location: { lat: 104.028553, lng: 30.660076 },
+  },
+  {
+    title: 'Jinsha Museum',
+    location: { lat: 104.012659, lng: 30.681726 },
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,49 +1,93 @@
-/* global ko, google, document */
+/* global ko, google, document, mapStyle, locationData */
 
 let map;
 
 const markers = [];
-// my fake datas
-const locationData = [
-  {
-    title: '天府广场',
-    location: { lat: 104.065837, lng: 30.657349 },
-  },
-  {
-    title: '武侯祠',
-    location: { lat: 104.047994, lng: 30.646094 },
-  },
-  {
-    title: '四川大学',
-    location: { lat: 104.083748, lng: 30.630869 },
-  },
-  {
-    title: '杜甫草堂',
-    location: { lat: 104.028553, lng: 30.660076 },
-  },
-  {
-    title: '金沙遗址博物馆',
-    location: { lat: 104.012659, lng: 30.681726 },
-  },
-];
 
 /**
  * init google map
  */
-function initMap() { // eslint-disable-line
+function initMap() {
+  // eslint-disable-line
   const chengdu = { lat: 104.065837, lng: 30.657349 };
   // create map
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: chengdu,
+    styles: mapStyle,
   });
+
+  // create infowindow
+  const infoWindow = new google.maps.InfoWindow();
+
+  // clean infowindow on close
+  infoWindow.addListener('closeclick', () => {
+    infoWindow.marker = null;
+  });
+
+  // create bounds
+  const bounds = new google.maps.LatLngBounds();
+
+  // customize marker appearance
+  const pinShadow = new google.maps.MarkerImage(
+    'http://chart.apis.google.com/chart?chst=d_map_pin_shadow',
+    new google.maps.Size(40, 37),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(12, 35),
+  );
+  const normalMarkerImage = makeMarkerWithColor('62efff');
+  const highlightMarkerImage = makeMarkerWithColor('008ba3');
+
   // create markers
-  locationData.forEach((data) => {
-    markers.push(new google.maps.Marker({
+  locationData.forEach((data, index) => {
+    const marker = new google.maps.Marker({
       position: data.location,
       map,
-    }));
+      title: data.title,
+      animation: google.maps.Animation.DROP,
+      id: index,
+      icon: normalMarkerImage,
+      shadow: pinShadow,
+    });
+    bounds.extend(marker.position);
+    // click marker to open info window.
+    marker.addListener('click', () => {
+      populateInfoWindow(marker, infoWindow);
+    });
+    // change marker's color on hover
+    marker.addListener('mouseover', () => {
+      marker.setIcon(highlightMarkerImage);
+    });
+    marker.addListener('mouseout', () => {
+      marker.setIcon(normalMarkerImage);
+    });
+    markers.push(marker);
   });
+
+  map.fitBounds(bounds);
+}
+
+function makeMarkerWithColor(color) {
+  const pinImage = new google.maps.MarkerImage(
+    `http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|${color}`,
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+  );
+  return pinImage;
+}
+
+/**
+ * Show the info window after clicking a marker
+ * @param {*} marker marker being clicked
+ * @param {*} infoWindow info window instance
+ */
+function populateInfoWindow(marker, infoWindow) {
+  if (infoWindow.marker !== marker) {
+    infoWindow.marker = marker; // eslint-disable-line
+    infoWindow.setContent('');
+    infoWindow.open(map, marker);
+  }
 }
 
 /**
@@ -52,6 +96,30 @@ function initMap() { // eslint-disable-line
 class ViewModal {
   constructor() {
     this.filterInput = ko.observable(); // value of location filter input
+    this.listItems = ko.observableArray(locationData.slice());
+  }
+
+  // User filter input event
+  onFilterChanged() {
+    // Assign a new array based on the filter.
+    this.listItems.removeAll();
+    const bounds = new google.maps.LatLngBounds();
+    locationData.forEach((location, index) => {
+      // get the marker related to this location
+      const marker = markers[index];
+      if (location.title.includes(this.filterInput())) {
+        // If the filter matches this location
+        this.listItems.push(location);
+        // set the specific marker to show
+        marker.setMap(map);
+        bounds.extend(marker.position);
+      } else {
+        // hide the marker
+        marker.setMap(null);
+      }
+    });
+    // replace the map so that all filtered markers are shown.
+    map.fitBounds(bounds);
   }
 }
 
